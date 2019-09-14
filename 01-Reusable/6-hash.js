dfffc3a25ee525057a4034e560eabc57a3bad47b
webenot <webenot@mail.ua>
2019-09-14 15:35:57 +0300

    Remove duplicated directory

--- 01-Reusable/6-hash.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const usersData = {
-  'Marcus Aurelius': '+380445554433'
-};
-
-const findPhoneByName = name => usersData[name];
-
-console.log(findPhoneByName('Marcus Aurelius'));
